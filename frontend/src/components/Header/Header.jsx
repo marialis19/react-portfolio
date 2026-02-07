d15c273a5e3476a3b4cb96d7585ee22f977134b8
Lis <168233558+marialis19@users.noreply.github.com>
2026-02-06 22:36:39 -0300

    Feat: foto de perfil

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,13 +1,21 @@
 import "./Header.css";
 
-function Header ({ name, role, stack }) {
-    return (
-        <header className="header">
-            <h1>{name}</h1>
-            <h2>{role}</h2>
-            <p>{stack} </p>
-        </header>
-    );
+function Header({ name, role, stack }) {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <div className="header-text">
+          <h1>{name}</h1>
+          <h2>{role}</h2>
+          <p>{stack}</p>
+        </div>
+
+        <div className="header-image">
+          <img src="/perfil_cv.png" alt="Foto de perfil" />
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
